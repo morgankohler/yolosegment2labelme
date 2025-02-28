ae5e42bb08a8304adcd3002793fb78f6bd6c41db
morgankohler <kohler.r.morgan@gmail.com>
2025-02-28 09:57:14 -0500

    changed main to accept a yolo config file for arbitrary arguments

--- yolosegment2labelme/yolosegment2labelme.py
+++ yolosegment2labelme/yolosegment2labelme.py
@@ -1,4 +1,5 @@
 import argparse
+import json
 from .polygon_saver import PolygonSaver
 from ultralytics import YOLO
 
@@ -6,8 +7,11 @@ def main():
     parser = argparse.ArgumentParser(description='Convert YOLO results to JSON files.')
     parser.add_argument('--model', default='yolov8n-seg.pt', help='Path to YOLO model weights file (default is yolov8n)')
     parser.add_argument('--images', required=True, help='Path to folder containing images')
-    parser.add_argument('--conf', type=float, default=0.2, help='Confidence threshold')
+    parser.add_argument('--config', required=False, help='Path to json file containing yolo prediction configuration')
     args = parser.parse_args()
+
+    # Load YOLO arguments from config file
+    yolo_arguments = json.load(open(args.config, 'r')) if args.config else {}
 
     # Use the input images directory as the output directory for JSON files
     output_dir = args.images
@@ -19,7 +23,7 @@ def main():
     yolo_model = YOLO(args.model)
 
     # Get results from YOLO model predictions
-    results = yolo_model.predict(args.images, save=True, conf=args.conf)
+    results = yolo_model.predict(args.images, save=True, **yolo_arguments)
 
     # Generate JSON files with results
     polygon_saver.generate_json_with_results(results, output_dir)
